60c7e134a09e1e95fcc60f34c02a527ed4963492
Laura Trigo <160795749+lauratrigo@users.noreply.github.com>
2026-03-07 22:28:27 -0300

    API em Python Atualizada para FastAPI

--- api.py
+++ api.py
@@ -2,40 +2,60 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from pymongo import MongoClient
 from datetime import datetime
-import uvicorn
+import os
 
 app = FastAPI()
 
-# Substitua pela sua String de Conexão do MongoDB Atlas
-MONGO_DETAILS = "mongodb+srv://ecocomp:<db_password>@cluster0.ictnsp1.mongodb.net/?appName=Cluster0"
+# conexão MongoDB
+MONGO_DETAILS = os.getenv("MONGO_URL")
+
 client = MongoClient(MONGO_DETAILS)
 db = client.ecocomp
 collection = db.sensor_data
 
-# Modelo de dados que a API espera receber
+
+# modelo dos dados recebidos
 class SensorData(BaseModel):
     temperatura: float
     umidadeAr: float
     umidadeSolo: float
 
+
+# rota POST para enviar dados
 @app.post("/dados")
 async def receber_dados(data: SensorData):
-    documento = data.dict()
-    documento["data_hora"] = datetime.now() # Adiciona o timestamp
-    
+
+    documento = {
+        "temperatura": data.temperatura,
+        "umidadeAr": data.umidadeAr,
+        "umidadeSolo": data.umidadeSolo,
+        "data_hora": datetime.utcnow()
+    }
+
     try:
-        res = collection.insert_one(documento)
-        return {"status": "sucesso", "id": str(res.inserted_id)}
+        resultado = collection.insert_one(documento)
+
+        return {
+            "status": "sucesso",
+            "id": str(resultado.inserted_id)
+        }
+
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
+# rota GET para pegar dados
 @app.get("/dados")
 async def pegar_dados():
-    # Retorna os últimos 10 registros
+
     dados = list(collection.find().sort("data_hora", -1).limit(10))
+
     for d in dados:
         d["_id"] = str(d["_id"])
+
     return dados
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+@app.get("/")
+async def home():
+    return {"status": "API EcoComp rodando"}
